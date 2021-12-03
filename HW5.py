0962589ac0db8ce965c740c9d4f161f6804b603c
nikden34562 <nikolasmongrail@gmail.com>
2021-12-03 21:52:08 +0300

    H_W_5

---
+++ HW5.py
@@ -0,0 +1,4 @@
+Mother = ('Grandfather', 'Grandmother')
+Father = ('Grandpa', 'Grandma')
+RELATIVES = ("I", "Brother", Mother, Father)
+print("My family consists of", len(RELATIVES)+len(Father)+len(Mother), "members")
