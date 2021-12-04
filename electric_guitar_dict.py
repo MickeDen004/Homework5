edc921906a143263de8b8371adb5e0d16989b693
nikden34562 <nikolasmongrail@gmail.com>
2021-12-04 12:23:35 +0300

    first basic dict

---
+++ electric_guitar_dict.py
@@ -0,0 +1,18 @@
+character = {'Manufacturer': 'ESP LTD',
+                   'Model': 'Arrow-1000 VLAND',
+                   'Price': '£1,025',
+                   'Shape': 'V-Style',
+                   'Soundboard': 'Mahogany',
+                   'Neck': 'Maple',
+                   'Frets': '24',
+                   'Pickups': 'HH',
+                   'Tremolo': FR-Style',
+                   'Colour': 'Violet Andromeda',
+                   'Fretboard': 'Macassar ebony',
+                   'Scale': '648 mm',
+                   'Item number': '459234',
+                   
+                   'Available': 3}
+                   
+
+         
