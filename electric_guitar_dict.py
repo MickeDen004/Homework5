2a9e5e7fe8bb9ae91f877588a5f82b08abaf65f6
nikden34562 <nikolasmongrail@gmail.com>
2021-12-04 12:37:35 +0300

    character_2 added

--- electric_guitar_dict.py
+++ electric_guitar_dict.py
@@ -1,4 +1,4 @@
-character = {'Manufacturer': 'ESP LTD',
+character_1 = {'Manufacturer': 'ESP LTD',
                    'Model': 'Arrow-1000 VLAND',
                    'Price': '£1,025',
                    'Shape': 'V-Style',
@@ -6,13 +6,31 @@ character = {'Manufacturer': 'ESP LTD',
                    'Neck': 'Maple',
                    'Frets': '24',
                    'Pickups': 'HH',
-                   'Tremolo': FR-Style',
+                   'Tremolo': 'FR-Style',
                    'Colour': 'Violet Andromeda',
                    'Fretboard': 'Macassar ebony',
                    'Scale': '648 mm',
-                   'Item number': '459234',
+                   'Included case': Yes,
+                   'Item number': '659304',
+                   
+                   'Available': 5}
+
+character_2 =     {'Manufacturer': 'Gibson',
+                   'Model': 'Custom EB GH',
+                   'Price': '£3,899',
+                   'Shape': 'Explorer',
+                   'Soundboard': 'Mahogany',
+                   'Neck': 'Mahogany',
+                   'Frets': '22',
+                   'Pickups': 'HH',
+                   'Tremolo': 'None',
+                   'Colour': 'Ebony',
+                   'Fretboard': 'Richlite',
+                   'Scale': '628 mm',
+                   'Included case': Yes,
+                   'Item number': '713078',
                    
                    'Available': 3}
-                   
+
 
          
