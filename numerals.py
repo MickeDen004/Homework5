d36e6de0bd32f20d19f143836b9d30e9907e7a6d
nikden34562 <nikolasmongrail@gmail.com>
2021-12-04 18:16:51 +0300

    numerals values

---
+++ numerals.py
@@ -0,0 +1,14 @@
+numerals = {1: 'I',
+            2: 'II',
+            3: 'III',
+            4: 'IV',
+            5: 'V',
+            6: 'VI',
+            7: 'VII',
+            8: 'VIII',
+            9: 'IX',
+            10: 'X'
+            }
+numerals[1]
+numerals[4]
+numerals[8]
